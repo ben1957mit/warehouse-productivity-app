0015c28a74480c92fb3e04386762e16c84da8e18
ben1957mit <ben1957mit@gmail.com>
2026-09-07 20:19:43 -0600

    Fix formatting of firebase_config in main.py

--- main.py
+++ main.py
@@ -389,18 +389,16 @@ import pyrebase
 # FIREBASE CLIENT CONFIG (Frontend Only)
 # -------------------------------------------------
 firebase_config = {
-firebase_config = {
     "apiKey": "YOUR_FIREBASE_API_KEY",
     "authDomain": "YOUR_FIREBASE_AUTH_DOMAIN",
     "projectId": "YOUR_FIREBASE_PROJECT_ID",
     "storageBucket": "YOUR_FIREBASE_STORAGE_BUCKET",
     "messagingSenderId": "YOUR_FIREBASE_SENDER_ID",
     "appId": "YOUR_FIREBASE_APP_ID",
-    "databaseURL": ""  
+    "databaseURL": ""   # required by pyrebase4
 }
 
 }
-
 firebase = pyrebase.initialize_app(firebase_config)
 auth = firebase.auth()
 
